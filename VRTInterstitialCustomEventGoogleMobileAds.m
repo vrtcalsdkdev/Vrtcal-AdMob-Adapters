292a885442a13e044c09c5b3e7752488842dd432
Scott McCoy <scotthmccoy@gmail.com>
2021-05-26 15:02:09 -0700

    feat: Updated AdMob adapter to new API

--- VRTInterstitialCustomEventGoogleMobileAds.m
+++ VRTInterstitialCustomEventGoogleMobileAds.m
@@ -40,41 +40,37 @@
 - (void) showInterstitialAd {
     UIViewController *vc = [self.viewControllerDelegate vrtViewControllerForModalPresentation];
     [self.gadInterstitial presentFromRootViewController:vc];
+    [self.customEventShowDelegate customEventWillPresentModal:VRTModalTypeInterstitial];
 }
 
 
 #pragma mark Display-Time Lifecycle Notifications
 
-/// Called just before presenting an interstitial. After this method finishes the interstitial will
-/// animate onto the screen. Use this opportunity to stop animations and save the state of your
-/// application in case the user leaves while the interstitial is on screen (e.g. to visit the App
-/// Store from a link on the interstitial).
-- (void)interstitialWillPresentScreen:(GADInterstitialAd *)ad {
+
+/// Tells the delegate that an impression has been recorded for the ad.
+- (void)adDidRecordImpression:(nonnull id<GADFullScreenPresentingAd>)ad {
+    //There is no Vrtcal analog for this event
+}
+
+/// Tells the delegate that the ad failed to present full screen content.
+- (void)ad:(nonnull id<GADFullScreenPresentingAd>)ad didFailToPresentFullScreenContentWithError:(nonnull NSError *)error {
+    //There is no Vrtcal analog for this event
+}
+
+/// Tells the delegate that the ad presented full screen content.
+- (void)adDidPresentFullScreenContent:(nonnull id<GADFullScreenPresentingAd>)ad {
     [self.customEventShowDelegate customEventShown];
+    [self.customEventShowDelegate customEventDidPresentModal:VRTModalTypeInterstitial];
 }
 
-/// Called when |ad| fails to present.
-- (void)interstitialDidFailToPresentScreen:(GADInterstitialAd *)ad {
-    
-}
-
-/// Called before the interstitial is to be animated off the screen.
-- (void)interstitialWillDismissScreen:(GADInterstitialAd *)ad {
+/// Tells the delegate that the ad will dismiss full screen content.
+- (void)adWillDismissFullScreenContent:(nonnull id<GADFullScreenPresentingAd>)ad {
     [self.customEventShowDelegate customEventWillDismissModal:VRTModalTypeInterstitial];
 }
 
-/// Called just after dismissing an interstitial and it has animated off the screen.
-- (void)interstitialDidDismissScreen:(GADInterstitialAd *)ad {
+/// Tells the delegate that the ad dismissed full screen content.
+- (void)adDidDismissFullScreenContent:(nonnull id<GADFullScreenPresentingAd>)ad {
     [self.customEventShowDelegate customEventDidDismissModal:VRTModalTypeInterstitial];
 }
-
-/// Called just before the application will background or terminate because the user clicked on an
-/// ad that will launch another application (such as the App Store). The normal
-/// UIApplicationDelegate methods, like applicationDidEnterBackground:, will be called immediately
-/// before this.
-- (void)interstitialWillLeaveApplication:(GADInterstitialAd *)ad {
-    [self.customEventShowDelegate customEventWillLeaveApplication];
-}
-
 
 @end
